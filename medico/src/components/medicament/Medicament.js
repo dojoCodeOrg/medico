9ad2e77c7db82951f3f9e8dc96682af7eb4b0bb9
yayahc <hamedcuenca5@gmail.com>
2023-01-12 15:32:46 +0000

    can get medoc from firebase after creating

--- medico/src/components/medicament/Medicament.js
+++ medico/src/components/medicament/Medicament.js
@@ -1,7 +1,83 @@
+import React, { useEffect, useState } from "react";
+import { auth,db,stopNetworkAcces } from "../../firebase";
+import { useNavigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { query, collection, getDocs } from "firebase/firestore";
+
 function Medicament() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [user, loading] = useAuthState(auth);
+    const navigate = useNavigate();
+
+
+    let medicamentID = null;
+    try {
+        medicamentID = window.location.href.split('?')[1].split('!')[0];
+    } catch (error) {
+        console.log(error)
+    }
+    
+    function corMonth(m) {
+        let finalMonth = null;
+        const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        month.forEach((i) => {
+            if (m == month.indexOf(i)) {
+                finalMonth = i;
+            }
+        })
+        return finalMonth
+    }
+
+    const firebaseTimeToDayMonthYearAndHourMinutes = async (time) => {
+        const questionTime = time.getHours()+':'+time.getMinutes();
+        const questionDate = time.getDate()+' '+corMonth(time.getMonth())+', '+time.getFullYear();
+        return questionDate+' a '+questionTime;
+    }
+
+    const fetchMedicament = async () => {
+        let medicament = [];
+        try {
+            const q = query(collection(db, "pharmacies"));
+            const doc = await getDocs(q);
+            const data = doc.docs;
+            data.forEach((item) => {
+                if (!item.data().medicament) {
+                    item.data().medicament = {};
+                }
+                const tempMedicament = item.data().medicament;
+                medicament.push(tempMedicament);
+            });
+            medicament = medicament[0];
+            console.log(medicament);
+
+            let medoc_area = document.querySelector('#medoc_area');
+            medoc_area.innerHTML = "";
+
+            medicament.forEach((item) => {
+                if (Object.keys(item)[0] === medicamentID) {                    
+                    let medoc_name = document.createElement('div');
+                    medoc_name.classList.add('medoc');
+                    medoc_name.innerHTML = item[0].name;
+                    medoc_area.appendChild(medoc_name);
+                };
+            });
+        } catch (error) {
+            console.log(error);
+        }  
+    }
+
+    useEffect(() => {        
+        if (loading) return;
+        if (!user) navigate("/sign");
+
+        fetchMedicament();
+
+    }, [user, loading]);
+
     return (
         <>
         Medicament
+        <div id="medoc_area"></div>
         </>
     )
 }
