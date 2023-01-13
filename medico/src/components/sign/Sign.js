811732afb4d56bda680805afcc9b37a16a4a245a
yayahc <hamedcuenca5@gmail.com>
2023-01-13 13:57:18 +0000

    merge sam home.html & home.css to sign components

--- medico/src/components/sign/Sign.js
+++ medico/src/components/sign/Sign.js
@@ -4,6 +4,9 @@ import { useNavigate } from "react-router-dom";
 
 import { signInWithGoogle,auth } from "../../firebase";
 
+import group1 from "../../assets/images/Group 1.svg";
+import group2 from "../../assets/images/Group 2.svg";
+import "./sign.css";
 
 function Sign() {
     const [user, loading] = useAuthState(auth);
@@ -23,9 +26,28 @@ function Sign() {
 
 
     return (
-      <>
-        <button onClick={signClient}>client</button>        
-        <button onClick={signPharmacie}>Pharmacie</button>    
+      <>       
+
+        <div class="container">
+                <main>
+                    <div class="client">
+                        <div class="image">
+                            <img src={group1} alt="" />
+                        </div>
+                        <h1>Medico Client</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, voluptatibus commodi animi dolore reprehenderit, minus in natus id, culpa quam eligendi iusto amet nesciunt quisquam deserunt soluta atque aspernatur quos!</p>
+                        <button>Client</button>
+                    </div>
+                    <div class="pharmacie">
+                        <div class="image">
+                            <img src={group2} alt="" />
+                        </div>
+                        <h1>Medico Pharmacie</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, voluptatibus commodi animi dolore reprehenderit, minus in natus id, culpa quam eligendi iusto amet nesciunt quisquam deserunt soluta atque aspernatur quos!</p>
+                        <button>Pharmacie</button>
+                    </div>
+                </main>
+        </div>
       </>
     )
 }
