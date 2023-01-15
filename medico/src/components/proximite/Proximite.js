7035f54958f4088fb9051897325f0c843990cff8
yayahc <hamedcuenca5@gmail.com>
2023-01-15 23:58:04 +0000

    just updating some render

--- medico/src/components/proximite/Proximite.js
+++ medico/src/components/proximite/Proximite.js
@@ -3,7 +3,7 @@ import "./proximite.css";
 function Proximite() {
     return (
         <>
-            <iframe width="100%" height="640vh" frameborder="0" allowfullscreen src="//umap.openstreetmap.fr/no/map/carte-des-pharmacies_638808?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=false&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=false&onLoadPanel=databrowser&captionBar=false"></iframe>
+            <iframe width="100%" frameborder="0" allowfullscreen src="//umap.openstreetmap.fr/no/map/carte-des-pharmacies_638808?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=false&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=false&onLoadPanel=databrowser&captionBar=false"></iframe>
         </>
     )
 }
