f40223ac6a48825751b69d043ea56e6478e3c0db
yayahc <hamedcuenca5@gmail.com>
2023-01-15 20:37:34 +0000

    just upd privacy

--- medico/src/components/notFound/NotFound.js
+++ medico/src/components/notFound/NotFound.js
@@ -11,7 +11,7 @@ function NotFound() {
 
             <div className="not-found-content">
                 {/* <center><img src={notf} /></center> */}
-                <h1>Page Not Found</h1>
+                <h1>Page Not Found</h1><br></br>
                 <p>Sorry, the page you are looking for could not be found. It may have been moved, deleted, or the URL may have been typed incorrectly.
                 Please check the URL and try again. If you continue to experience issues, please contact the site administrator.</p>
             </div>
--- medico/src/components/privacyPolicy/privacyPolicy.js
+++ medico/src/components/privacyPolicy/privacyPolicy.js
@@ -20,29 +20,29 @@ function Privacy() {
             <Header />
            
            <div className="privacy">
-           <h1>Termes of use</h1>
+           <h1>Termes of use</h1><br></br>
             <span>Last updated {state.curDT}</span>
 
-            <h2>1. AGREEMENT TO TERMS</h2>
-            <p>These Terms of Use constitute a legally binding agreement made between you, whether personally or on behalf of an entity (“you”) and arche ("Company," “we," “us," or “our”), concerning your access to and use of the arche website as well as any other media form, media channel, mobile website or mobile application related, linked, or otherwise connected thereto (collectively, the “Site”). You agree that by accessing the Site, you have read, understood, and agreed to be bound by all of these Terms of Use. IF YOU DO NOT AGREE WITH ALL OF THESE TERMS OF USE, THEN YOU ARE EXPRESSLY PROHIBITED FROM USING THE SITE AND YOU MUST DISCONTINUE USE IMMEDIATELY. Supplemental terms and conditions or documents that may be posted on the Site from time to time are hereby expressly incorporated herein by reference. We reserve the right, in our sole discretion, to make changes or modifications to these Terms of Use at any time and for any reason. We will alert you about any changes by updating the “Last updated” date of these Terms of Use, and you waive any right to receive specific notice of each such change. Please ensure that you check the applicable Terms every time you use our Site so that you understand which Terms apply. You will be subject to, and will be deemed to have been made aware of and to have accepted, the changes in any revised Terms of Use by your continued use of the Site after the date such revised Terms of Use are posted. The information provided on the Site is not intended for distribution to or use by any person or entity in any jurisdiction or country where such distribution or use would be contrary to law or regulation or which would subject us to any registration requirement within such jurisdiction or country. Accordingly, those persons who choose to access the Site from other locations do so on their own initiative and are solely responsible for compliance with local laws, if and to the extent local laws are applicable. All users who are minors in the jurisdiction in which they reside (generally under the age of 18) must have the permission of, and be directly supervised by, their parent or guardian to use the Site. If you are a minor, you must have your parent or guardian read and agree to these Terms of Use prior to you using the Site.</p>
+            <br></br><h2>1. AGREEMENT TO TERMS</h2><br></br>
+            <p>These Terms of Use constitute a legally binding agreement made between you, whether personally or on behalf of an entity (“you”) and medico ("Company," “we," “us," or “our”), concerning your access to and use of the medico website as well as any other media form, media channel, mobile website or mobile application related, linked, or otherwise connected thereto (collectively, the “Site”). You agree that by accessing the Site, you have read, understood, and agreed to be bound by all of these Terms of Use. IF YOU DO NOT AGREE WITH ALL OF THESE TERMS OF USE, THEN YOU ARE EXPRESSLY PROHIBITED FROM USING THE SITE AND YOU MUST DISCONTINUE USE IMMEDIATELY. Supplemental terms and conditions or documents that may be posted on the Site from time to time are hereby expressly incorporated herein by reference. We reserve the right, in our sole discretion, to make changes or modifications to these Terms of Use at any time and for any reason. We will alert you about any changes by updating the “Last updated” date of these Terms of Use, and you waive any right to receive specific notice of each such change. Please ensure that you check the applicable Terms every time you use our Site so that you understand which Terms apply. You will be subject to, and will be deemed to have been made aware of and to have accepted, the changes in any revised Terms of Use by your continued use of the Site after the date such revised Terms of Use are posted. The information provided on the Site is not intended for distribution to or use by any person or entity in any jurisdiction or country where such distribution or use would be contrary to law or regulation or which would subject us to any registration requirement within such jurisdiction or country. Accordingly, those persons who choose to access the Site from other locations do so on their own initiative and are solely responsible for compliance with local laws, if and to the extent local laws are applicable. All users who are minors in the jurisdiction in which they reside (generally under the age of 18) must have the permission of, and be directly supervised by, their parent or guardian to use the Site. If you are a minor, you must have your parent or guardian read and agree to these Terms of Use prior to you using the Site.</p>
 
 
-            <h2>2. INTELLECTUAL PROPERTY RIGHTS</h2>
+            <br></br><h2>2. INTELLECTUAL PROPERTY RIGHTS</h2><br></br>
             <p>Unless otherwise indicated, the Site is our proprietary property and all source code, databases, functionality, software, website designs, audio, video, text, photographs, and graphics on the Site (collectively, the “Content”) and the trademarks, service marks, and logos contained therein (the “Marks”) are owned or controlled by us or licensed to us, and are protected by copyright and trademark laws and various other intellectual property rights and unfair competition laws of the United States, international copyright laws, and international conventions. The Content and the Marks are provided on the Site “AS IS” for your information and personal use only. Except as expressly provided in these Terms of Use, no part of the Site and no Content or Marks may be copied, reproduced, aggregated, republished, uploaded, posted, publicly displayed, encoded, translated, transmitted, distributed, sold, licensed, or otherwise exploited for any commercial purpose whatsoever, without our express prior written permission. Provided that you are eligible to use the Site, you are granted a limited license to access and use the Site and to download or print a copy of any portion of the Content to which you have properly gained access solely for your personal, non-commercial use. We reserve all rights</p>
 
 
             
-            <h2>3. USER REPRESENTATIONS</h2>
+            <br></br><h2>3. USER REPRESENTATIONS</h2><br></br>
             <p>By using the Site, you represent and warrant that: (1) all registration information you submit will be true, accurate, current, and complete; (2) you will maintain the accuracy of such information and promptly update such registration information as necessary; (3) you have the legal capacity and you agree to comply with these Terms of Use; (4) you are not a minor in the jurisdiction in which you reside, or if a minor, you have received parental permission to use the Site; (5) you will not access the Site through automated or non-human means, whether through a bot, script, or otherwise; (6) you will not use the Site for any illegal or unauthorized purpose; and (7) your use of the Site will not violate any applicable law or regulation. If you provide any information that is untrue, inaccurate, not current, or incomplete, we have the right to suspend or terminate your account and refuse any and all current or future use of the Site (or any portion thereof)..</p>
             
 
 
-            <h2>4. USER REGISTRATION</h2>
+            <br></br><h2>4. USER REGISTRATION</h2><br></br>
             <p>You may be required to register with the Site. You agree to keep your password confidential and will be responsible for all use of your account and password. We reserve the right to remove, reclaim, or change a username you select if we determine, in our sole discretion, that such username is inappropriate, obscene, or otherwise objectionable.</p>
 
 
 
-            <h2>5. PROHIBITED ACTIVITIES</h2>
+            <br></br><h2>5. PROHIBITED ACTIVITIES</h2><br></br>
             <p>You may not access or use the Site for any purpose other than that for which we make the Site available. The Site may not be used in connection with any commercial endeavors except those that are specifically endorsed or approved by us. As a user of the Site, you agree not to: <br /></p>
 
             
@@ -91,16 +91,16 @@ function Privacy() {
             </div>
             
 
-            <h2>6. USER GENERATED CONTRIBUTIONS</h2>
+            <br></br><h2>6. USER GENERATED CONTRIBUTIONS</h2><br></br>
             <p>The Site may invite you to chat, contribute to, or participate in blogs, message boards, online forums, and other functionality, and may provide you with the opportunity to create, submit, post, display, transmit, perform, publish, distribute, or broadcast content and materials to us or on the Site, including but not limited to text, writings, video, audio, photographs, graphics, comments, suggestions, or personal information or other material (collectively, "Contributions"). Contributions may be viewable by other users of the Site and through third-party websites.</p>
             
 
-            <h2>7. USER DATA</h2>
+            <br></br><h2>7. USER DATA</h2><br></br>
             <p>We will maintain certain data that you transmit to the Site for the purpose of managing the performance of the Site, as well as data relating to your use of the Site. Although we perform regular routine backups of data, you are solely responsible for all data that you transmit or that relates to any activity you have undertaken using the Site. You agree that we shall have no liability to you for any loss or corruption of any such data, and you hereby waive any right of action against us arising from any such loss or corruption of such data.</p>
             
             
-            <h2>8. CONTACT US</h2>
-            <p>In order to resolve a complaint regarding the Site or to receive further information regarding use of the Site, please contact us at: <br /> arche <br /> Abidajn 8922<br /> Cote d'Ivoire<br /> Phone: 0779186972</p>
+            <br></br><h2>8. CONTACT US</h2><br></br>
+            <p>In order to resolve a complaint regarding the Site or to receive further information regarding use of the Site, please contact us at: <br /> medico <br /> Abidajn 8922<br /> Cote d'Ivoire<br /> Phone: 0779186972</p>
 
            </div>
 
