faa433f8ba5cace9dab7d2734b79bff4b674d3ea
yayahc <hamedcuenca5@gmail.com>
2023-01-17 21:20:39 +0000

    .

--- medico/src/App.js
+++ medico/src/App.js
@@ -10,6 +10,7 @@ import CreateMedicament from "./components/medicament/CreateMedicaments"
 import Privacy from "./components/privacyPolicy/privacyPolicy";
 import NotFound from "./components/notFound/NotFound";
 import Proximite from "./components/proximite/Proximite";
+import Medicaments from "./components/medicament/Medicaments";
 
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
           <Route exact path="/landing" element={<Landing />} />   
           <Route exact path="/sign" element={<Sign />} />
           <Route exact path="/medicament" element={<Medicament />} />
+          <Route exact path="/medicaments" element={<Medicaments />} />
           <Route exact path="/privacy" element={<Privacy />} />
           <Route exact path="/new/medicament" element={<CreateMedicament />} />
           <Route exact path="/user" element={<Dashboard />} />
